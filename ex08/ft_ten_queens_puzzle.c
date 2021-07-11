7789579d5284a32b7abf4b7d86f0248cde317f84
Juan Porta <jporta@c3r8s5.42madrid.com>
2021-07-11 14:17:12 +0200

    ultima subida 5

--- ex08/ft_ten_queens_puzzle.c
+++ ex08/ft_ten_queens_puzzle.c
@@ -6,7 +6,7 @@
 /*   By: jporta <jporta@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/08 17:37:52 by jporta            #+#    #+#             */
-/*   Updated: 2021/07/08 17:38:28 by jporta           ###   ########.fr       */
+/*   Updated: 2021/07/11 14:15:51 by jporta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,9 @@
 
 int	ft_ten_queens_puzzle(void)
 {
+    hola mi querid@ correct@r te escribo este mensaje agradecido encarecdamente
+    y siento haberle decepcionado por no haber entregado este ejercicio que supone un reto 
+    espero que tenga suerte en sus proyectos y con la molinete un saludo.
 
+    PD: la soluciion es, que la unica reina eres tu<3.
 }
