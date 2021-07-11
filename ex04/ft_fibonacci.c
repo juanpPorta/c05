fd49727a431120940d547317165071c0a001fb8b
Juan Porta <jporta@c3r8s5.42madrid.com>
2021-07-11 03:08:08 +0200

    subida ultima c05

--- ex04/ft_fibonacci.c
+++ ex04/ft_fibonacci.c
@@ -6,27 +6,26 @@
 /*   By: jporta <jporta@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/08 15:33:37 by jporta            #+#    #+#             */
-/*   Updated: 2021/07/08 16:49:07 by jporta           ###   ########.fr       */
+/*   Updated: 2021/07/11 03:06:20 by jporta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include<unistd.h>
 #include<stdio.h>
 
-int ft_fibonacci(int index)
+int	ft_fibonacci(int index)
 {
-    int number;
+	int	number;
 
-    if (index == 0)
-        return (0);
-    if (index == 1)
-        return (1);
-    if (index > 1 )
-    {
-        number = ft_fibonacci(index -1) + ft_fibonacci(index -2);
-        return(number);
-    }
-    else
-        return (-1);
-
+	if (index == 0)
+		return (0);
+	if (index == 1)
+		return (1);
+	if (index > 1 )
+	{
+		number = ft_fibonacci(index - 1) + ft_fibonacci(index - 2);
+		return (number);
+	}
+	else
+		return (-1);
 }
--- ex07/ft_find_next_prime.c
+++ ex07/ft_find_next_prime.c
@@ -6,7 +6,7 @@
 /*   By: jporta <jporta@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/08 17:15:07 by jporta            #+#    #+#             */
-/*   Updated: 2021/07/08 17:31:48 by jporta           ###   ########.fr       */
+/*   Updated: 2021/07/11 03:07:00 by jporta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,22 +37,4 @@ int	ft_find_next_prime(int nb)
 		i++;
 	}
 	return (nb);
-}
-
-int		main(void)
-{
-	printf("-10 = %d (2)\n", ft_find_next_prime(-10));
-	printf(" 0 = %d (2)\n", ft_find_next_prime(0));
-	printf(" 1 = %d (2)\n", ft_find_next_prime(1));
-	printf(" 2 = %d (2)\n", ft_find_next_prime(2));
-	printf(" 3 = %d (3)\n", ft_find_next_prime(3));
-	printf(" 4 = %d (5)\n", ft_find_next_prime(4));
-	printf(" 5 = %d (5)\n", ft_find_next_prime(5));
-	printf(" 6 = %d (7)\n", ft_find_next_prime(6));
-	printf(" 7 = %d (7)\n", ft_find_next_prime(7));
-	printf(" 8 = %d (11)\n", ft_find_next_prime(8));
-	printf(" 9 = %d (11)\n", ft_find_next_prime(9));
-	printf(" 10 = %d (11)\n", ft_find_next_prime(10));
-	printf(" 11 = %d (11)\n", ft_find_next_prime(11));
-	
 }
